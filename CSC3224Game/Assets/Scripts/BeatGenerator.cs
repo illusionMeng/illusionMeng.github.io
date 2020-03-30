7711dc098cfcfa6af5abae0aba1d388e0b430d9d
MengCheang <c.cheang@newcastle.ac.uk>
2020-03-30 16:27:54 +0800

    push

---
+++ CSC3224Game/Assets/Scripts/BeatGenerator.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class BeatStat
+{
+    private float locationInBeat;
+    private string type;
+    private bool isDouble;
+
+    public BeatStat(float locationInBeat, string type, bool isDouble)
+    {
+        this.locationInBeat = locationInBeat;
+        this.type = type;
+        this.isDouble = isDouble;
+    }
+
+    public float getLocation() { return locationInBeat; }
+    public string getType() { return type; }
+    public bool getIsDouble() { return isDouble; }
+};
+
+public class BeatGenerator : MonoBehaviour
+{
+    public GameObject Beat;
+    public GameObject DoubleBeat;
+
+    public TextAsset file;
+
+    private string input;
+
+    private GameObject temp;
+
+    private float currentBeat = -1;
+
+    private List<BeatStat> beatContiner = new List<BeatStat>();
+    private List<BeatStat> toBeDeleted = new List<BeatStat>();
+
+    //  private float ;
+
+    // Use this for initialization
+    void Start()
+    {
+        input = file.text;
+        string[] lines = input.Split('\n');
+        for(int i = 0; i<lines.Length; i++)
+        {
+            string[] temp = lines[i].Split(' ');
+            beatContiner.Add(new BeatStat(float.Parse(temp[0]), temp[1], bool.Parse(temp[2])));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameHandler.instance.start)
+        {
+            if (!(Mathf.Abs(currentBeat - GameHandler.instance.songPositionInBeats) <= 0.0001))
+            {
+                float velocity = GameHandler.instance.speedMultiplier * (GameHandler.instance.songBpm / 60f);
+                float timeDiff = (GameHandler.instance.songPosition - GameHandler.instance.lastBeatPosition) - (GameHandler.instance.secPerBeat / 4);
+                float offset = velocity * timeDiff;
+
+                for (int i = 0; i < beatContiner.Count; i++)
+                {
+                    if (Mathf.Abs(beatContiner[i].getLocation() - GameHandler.instance.songPositionInBeats) <= 0.0001)
+                    {
+                        spawnBeat(offset, beatContiner[i].getType(), beatContiner[i].getIsDouble());
+                        toBeDeleted.Add(beatContiner[i]);
+                    }
+
+                }
+
+                for (int i = 0; i < toBeDeleted.Count; i++)
+                    beatContiner.Remove(toBeDeleted[i]);
+
+
+                toBeDeleted.Clear();
+
+                currentBeat = GameHandler.instance.songPositionInBeats;
+            }
+        }
+    }
+
+    void spawnBeat(float offset, string type, bool isDouble)
+    {
+
+        float distance = 9.875f + offset;
+        float rotation = 0;
+        Vector2 pos = new Vector2(0, 0);
+
+        if (!isDouble)
+        {
+            temp = Instantiate(Beat) as GameObject;
+            if (type == "u")
+            {
+                temp.GetComponent<BeatObject>().key1 = KeyCode.W;
+                temp.GetComponent<BeatObject>().key2 = KeyCode.I;
+
+                pos.y += distance;
+            }
+            if (type == "d")
+            {
+                temp.GetComponent<BeatObject>().key1 = KeyCode.S;
+                temp.GetComponent<BeatObject>().key2 = KeyCode.K;
+
+                pos.y -= distance;
+                rotation = 180;
+            }
+            if (type == "l")
+            {
+                temp.GetComponent<BeatObject>().key1 = KeyCode.A;
+                temp.GetComponent<BeatObject>().key2 = KeyCode.J;
+
+                pos.x -= distance;
+                rotation = 90;
+            }
+            if (type == "r")
+            {
+                temp.GetComponent<BeatObject>().key1 = KeyCode.D;
+                temp.GetComponent<BeatObject>().key2 = KeyCode.L;
+
+                pos.x += distance;
+                rotation = 270;
+            }
+        }
+        else
+        {
+            temp = Instantiate(DoubleBeat) as GameObject;
+            if (type == "u")
+            {
+                temp.GetComponent<DoubleBeatObject>().key1 = KeyCode.W;
+                temp.GetComponent<DoubleBeatObject>().key2 = KeyCode.I;
+
+                pos.y += distance;
+            }
+            if (type == "d")
+            {
+                temp.GetComponent<DoubleBeatObject>().key1 = KeyCode.S;
+                temp.GetComponent<DoubleBeatObject>().key2 = KeyCode.K;
+
+                pos.y -= distance;
+                rotation = 180;
+            }
+            if (type == "l")
+            {
+                temp.GetComponent<DoubleBeatObject>().key1 = KeyCode.A;
+                temp.GetComponent<DoubleBeatObject>().key2 = KeyCode.J;
+
+                pos.x -= distance;
+                rotation = 90;
+            }
+            if (type == "r")
+            {
+                temp.GetComponent<DoubleBeatObject>().key1 = KeyCode.D;
+                temp.GetComponent<DoubleBeatObject>().key2 = KeyCode.L;
+
+                pos.x += distance;
+                rotation = 270;
+            }
+        }
+
+        temp.transform.position = pos;
+        temp.transform.Rotate(0, 0, rotation);
+    }
+
+}
---
+++ CSC3224Game/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour
+{
+
+    public AudioSource music;
+    public float songBpm;
+    public float secondBpm;
+    public float bpmChangesFromBeat;
+    public float speedMultiplier;
+    public Text scoreText;
+    public Text comboText;
+    public Text judgeText;
+    public Text reminderText;
+
+    public static GameHandler instance;
+    public bool start;
+
+    public float secPerBeat;
+    public float songPosition = 0;
+    public float lastBeatPosition = 0;
+    public float songPositionInBeats = 0;
+    private float dspSongTime;
+
+    private int score = 0;
+    private int combo = 0;
+
+    private float lastChanged;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+        scoreText.text = "Score : 0";
+        comboText.text = "";
+        judgeText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!start)
+        {
+            if (Input.anyKeyDown)
+            {
+                foreach (Transform child in reminderText.transform)
+                {
+                    Destroy(child.gameObject);
+                }
+                
+                start = true;
+                secPerBeat = 60f / songBpm;
+
+                dspSongTime = (float)AudioSettings.dspTime;
+                music.Play();
+            }
+        }
+        else
+        {
+            songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+            if (songPosition - lastBeatPosition >= (secPerBeat / 4))
+            {
+                songPositionInBeats += 0.25f;
+                lastBeatPosition = songPosition - ((songPosition - lastBeatPosition) - (secPerBeat / 4));
+            }
+
+            if (songPositionInBeats == bpmChangesFromBeat && songBpm != secondBpm)
+            {
+                Debug.Log("BPM changed to " + secondBpm);
+                songBpm = secondBpm;
+                secPerBeat = 60f / songBpm;
+            }
+
+            if (Time.time - lastChanged >= 0.8)
+                judgeText.text = "";
+
+            if (songPositionInBeats >= 99.25)
+            {
+                music.Stop();
+            }
+        }
+    }
+
+    public void Hit(float disDiff)
+    {
+        float velocity = speedMultiplier * (songBpm / 60f);
+        float timeDiff = (disDiff / velocity) * 1000;
+
+        Debug.Log("Hit with offset " + timeDiff + "ms");
+
+        if(timeDiff <= 30)
+            Perfect();
+        else if (timeDiff <= 90)
+            Great();
+        else if (timeDiff <= 130)
+            Good();
+        else
+            Bad();
+
+        scoreText.text = "Score : " + score;
+
+        if (combo >= 5)
+            comboText.text = "" + combo;
+        else
+            comboText.text = "";
+
+        lastChanged = Time.time;
+    }
+
+    public void Perfect()
+    {
+        judgeText.text = "✦Perfect✦";
+        judgeText.color = new Color(218 / 255f, 189 / 255f, 81 / 255f);
+        score += 1000;
+        combo++;
+
+        Debug.Log("Perfect");
+    }
+
+    public void Great()
+    {
+        judgeText.text = "✣Great✣";
+        judgeText.color = new Color(164 / 255f, 81 / 255f, 174 / 255f);
+        score += 600;
+        combo++;
+
+        Debug.Log("Great");
+    }
+
+    public void Good()
+    {
+        judgeText.text = "✢Good✢";
+        judgeText.color = new Color(123 / 255f, 192 / 255f, 119 / 255f);
+        score += 300;
+        combo++;
+
+        Debug.Log("Good");
+    }
+
+    public void Bad()
+    {
+        judgeText.text = "-Bad-";
+        judgeText.color = new Color(52 / 255f, 82 / 255f, 180 / 255f);
+        combo = 0;
+
+        Debug.Log("Bad");
+    }
+
+    public void Miss()
+    { 
+        judgeText.text = "-Miss-";
+        judgeText.color = new Color(193 / 255f, 74 / 255f, 77 / 255f);
+        combo = 0;
+
+        Debug.Log("Miss");
+
+        comboText.text = "";
+
+        lastChanged = Time.time;
+    }
+}
